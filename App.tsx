761a73f02a7476f818cd73ec783645b4dfab6433
Murilo Morandi <murilomorandialexandre2011@gmail.com>
2021-10-26 20:40:44 -0300

    Configuração Inicial Projeto

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts, Poppins_400Regular,
+  Poppins_500Medium, Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+import { Dashboard } from './src/screen/Dashboard';
+import theme  from './src/Global/Styles/theme'
+
+export default function App() {
+  const [ fontsLoaded ] = useFonts(
+    {
+      Poppins_400Regular, Poppins_500Medium,
+      Poppins_700Bold
+    });
+    
+    if(!fontsLoaded){
+      return <AppLoading/>
+    }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard/>
+      
+    </ThemeProvider>
+  );
+}
---
+++ src/screen/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export const Dashboard: React.FC = (prop) => {
+  return(
+    <Container>
+      
+      <Title>Hellos</Title>
+    </Container>
+  ); 
+}
