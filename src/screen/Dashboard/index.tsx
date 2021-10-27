f6abd1913163ca7ddeddf7e8c557f3aa1ca37c7e
Murilo Morandi <murilomorandialexandre2011@gmail.com>
2021-10-27 18:51:48 -0300

    Criação do Header

--- src/screen/Dashboard/index.tsx
+++ src/screen/Dashboard/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
-import { Container, Title } from './styles';
+import { 
+  Container, Header,
+  UserWrapper, UserInfo,
+  Photo, User,
+  UserGreating, UserName,
+  Icon
+} from './styles';
 
-export const Dashboard: React.FC = (prop) => {
+export const Dashboard: React.FC = () => {
   return(
     <Container>
       
-      <Title>Hellos</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo 
+              source={{ uri:'https://avatars.githubusercontent.com/u/53803076?s=96&v=4' }}
+              />
+            <User>
+              <UserGreating>Olá</UserGreating>
+              
+              <UserName>Murilo</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power"/>
+        </UserWrapper>
+      </Header>
+
     </Container>
   ); 
 }
--- src/screen/Dashboard/styles.ts
+++ src/screen/Dashboard/styles.ts
@@ -1,19 +1,77 @@
 import styled from 'styled-components/native';
-//import theme  from '../../Global/Styles/theme';
+import {RFPercentage, RFValue} from 'react-native-responsive-fontsize'
+
+import { Feather } from  '@expo/vector-icons'
+
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
     align-items: center;
 
-    background-color: ${ ({ theme }) => theme.colors.background}; 
+    background-color: ${ ({ theme }) => theme.colors.background };  
 `;
 
-export const Title = styled.Text`
-    font-family: ${ ({ theme }) => theme.fonts.regular };
-    font-size: 24px;
-    font-weight: bold;
+export const Header = styled.View`
+    width: 100%;
+    height: ${RFPercentage(40)}px;
+    
+    background-color: ${ ({ theme }) => theme.colors.primary}; 
+    flex-direction: row;
+`;
 
-    color: ${({ theme }) => theme.colors.primary};
+export const UserWrapper = styled.View`
+    width: 100%;
+    
+    padding: 0px 24px;
+
+    flex-direction: row;
+    justify-content: space-between ;
+    align-items: center;
 
 `;
+
+
+//#region User infos
+
+export const UserInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const Photo = styled.Image`
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+
+    border-radius: 15px;
+`;
+
+export const User = styled.View`
+    flex-direction: column;
+
+    margin-left: 17px;
+    
+`;
+
+export const UserGreating = styled.Text`
+    color: ${ ({theme} ) => theme.colors.shape };
+
+    font-size: ${RFValue(18)};
+    font-family: ${ ({theme} ) => theme.fonts.regular };
+`;
+
+export const UserName = styled.Text`
+    color: ${ ({theme} )=> theme.colors.shape };
+
+    font-size: ${RFValue(18)};
+    font-family: ${ ({theme} )=> theme.fonts.bold };
+`;
+//#endregion
+
+//#region Icon Power
+export const Icon = styled(Feather)`
+    color: ${ ({ theme }) => theme.colors.secondary };
+    
+    font-size: ${RFValue(24)}px;
+
+`;
+//#endregion
